00e7db5c69723bb17eaa6ab93d6bf245c61f3796
Balamurali2002 <pvkbalawob1@gmail.com>
2026-01-03 01:54:09 +0530

    DSA-Bootcamp-JS

---
+++ ContainsDuplicate.js
@@ -0,0 +1,11 @@
+function containsDuplicate(nums) {
+    const seen = new set();
+    for ( let i = 0; i< nums.length; i++) {
+        if (seen.has(nums[i])) {
+            return true;
+        }
+        seen.add(nums[i]);
+    }
+    return false;
+}
+
